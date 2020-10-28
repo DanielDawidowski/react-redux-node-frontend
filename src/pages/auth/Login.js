afecd36227139019f37dde378add45fa4986f194
Daniel Dawidowski <dvds1987@gmail.com>
2020-10-28 14:20:05 +0100

    Redirect logged in user

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth, googleAuthProvider } from "../../firebase";
 import { toast } from "react-toastify";
 import { Button } from "antd";
 import { MailOutlined, GoogleOutlined } from "@ant-design/icons";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Login = ({ history }) => {
   const [email, setEmail] = useState("dvds1987@gmail.com");
   const [password, setPassword] = useState("Dawdaniel1987");
   const [loading, setLoading] = useState(false);
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    if (user && user.token) history.push("/");
+  }, [user]);
 
   let dispatch = useDispatch();
 
