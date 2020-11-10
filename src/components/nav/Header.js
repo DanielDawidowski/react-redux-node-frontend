f403741d4798964aa84c6ffe7bd6d235cb965398
Daniel Dawidowski <dvds1987@gmail.com>
2020-11-10 13:25:59 +0100

    Dynamic Dashboard Link

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -43,19 +43,15 @@ const Header = () => {
       </Item>
 
       {!user && (
-        <>
-          <Item
-            key="register"
-            icon={<UserAddOutlined />}
-            className="float-right"
-          >
-            <Link to="/register">Register</Link>
-          </Item>
+        <Item key="register" icon={<UserAddOutlined />} className="float-right">
+          <Link to="/register">Register</Link>
+        </Item>
+      )}
 
-          <Item key="login" icon={<UserOutlined />} className="float-right">
-            <Link to="/login">Login</Link>
-          </Item>
-        </>
+      {!user && (
+        <Item key="login" icon={<UserOutlined />} className="float-right">
+          <Link to="/login">Login</Link>
+        </Item>
       )}
 
       {user && (
@@ -64,8 +60,18 @@ const Header = () => {
           title={user.email && user.email.split("@")[0]}
           className="float-right"
         >
-          <Item key="setting:1">Option 1</Item>
-          <Item key="setting:2">Option 2</Item>
+          {user && user.role === "subscriber" && (
+            <Item>
+              <Link to="/user/history">Dashboard</Link>
+            </Item>
+          )}
+
+          {user && user.role === "admin" && (
+            <Item>
+              <Link to="/admin/dashboard">Dashboard</Link>
+            </Item>
+          )}
+
           <Item icon={<LogoutOutlined />} onClick={logout}>
             Logout
           </Item>
