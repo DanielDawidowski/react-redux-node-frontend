e56213bcc9005e71c230aba298b0a62a093368b8
unknown <dvds1987@gmail.com>
2020-12-28 12:06:28 +0100

    Redirect to Intended Page

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -15,16 +15,27 @@ const Login = ({ history }) => {
   const { user } = useSelector((state) => ({ ...state }));
 
   useEffect(() => {
-    if (user && user.token) history.push("/");
+    let intended = history.location.state;
+    if (intended) {
+      return;
+    } else {
+      if (user && user.token) history.push("/");
+    }
   }, [user, history]);
 
   let dispatch = useDispatch();
 
   const roleBasedRedirect = (res) => {
-    if (res.data.role === "admin") {
-      history.push("/admin/dashboard");
+    // check if intended
+    let intended = history.location.state;
+    if (intended) {
+      history.push(intended.from);
     } else {
-      history.push("/user/history");
+      if (res.data.role === "admin") {
+        history.push("/admin/dashboard");
+      } else {
+        history.push("/user/history");
+      }
     }
   };
 
@@ -163,6 +174,7 @@ const Login = ({ history }) => {
 };
 
 export default Login;
+
 
 // const [email, setEmail] = useState("dvds1987@gmail.com");
 // const [password, setPassword] = useState("Dawdaniel1987");
